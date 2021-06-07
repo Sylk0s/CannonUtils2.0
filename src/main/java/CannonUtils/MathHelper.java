4c90c09f4242ca33f79e61df2b6b057b16c93cfc
Ellie <intricatebread@gmail.com>
2021-06-06 23:28:44 -0400

    added a space

--- src/main/java/CannonUtils/MathHelper.java
+++ src/main/java/CannonUtils/MathHelper.java
@@ -1,6 +1,6 @@
 // Mojank's strange math class, important because they do a bunch of casting that produces float errors
 public final class MathHelper {
     public static float sqrt(double d) {
-        return (float)Math.sqrt(d);
+        return (float) Math.sqrt(d);
     }
 }
