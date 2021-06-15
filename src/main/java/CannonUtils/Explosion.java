67f57d6561b32b4936248653f07a105da995af19
Ellie <intricatebread@gmail.com>
2021-06-15 02:38:19 -0400

    Add getVelocityOptimized bc mojang code is cringe

--- src/main/java/CannonUtils/Explosion.java
+++ src/main/java/CannonUtils/Explosion.java
@@ -39,7 +39,26 @@ public final class Explosion {
         return new Vec3d(0, 0, 0);
     }
 
+    public Vec3d getVelocityOptimized(Vec3d pos, double eyeHeight, float exposure, boolean tnt) {
+        // Get distance from explosion to entity feet
+        double distanceNormalized = (double) (pos.distanceTo(this.pos) / (this.power * 2.0F));
+        // Exit if distance is too high
+        if (distanceNormalized > 1.0D) return new Vec3d(0, 0, 0);
+        // Get direction vector, different for tnt for some reason
+        Vec3d dir = new Vec3d(pos.x - this.pos.x, (tnt ? pos.y : pos.y + eyeHeight) - this.pos.y, pos.z - this.pos.z);
+        // Exit if direction vector is 0
+        if(dir.length() == 0.0D) return new Vec3d(0, 0, 0);
+        // Normalize direction vector
+        dir = dir.multiply(1 / dir.length());
+        // Velocity magnitude is (1 - distance/2power) * exposure
+        return dir.multiply((1.0D - distanceNormalized) * (double) exposure);
+    }
+
     public Vec3d getVelocity(Entity entity, float exposure) {
         return getVelocity(entity.pos, entity.eyeHeight, exposure, entity instanceof TntEntity);
+    }
+
+    public Vec3d getVelocityOptimized(Entity entity, float exposure) {
+        return getVelocityOptimized(entity.pos, entity.eyeHeight, exposure, entity instanceof TntEntity);
     }
 }
--- src/main/java/CannonUtils/Main.java
+++ src/main/java/CannonUtils/Main.java
@@ -4,34 +4,35 @@ package CannonUtils;
 
 public class Main {
     public static void main(String[] args) {
-        Explosion explosion = new Explosion(0, 1.061250001192092896, 0, 4.0F);
-        TntEntity tntEntity = new TntEntity(new Vec3d(1F/16F, 1.061250001192093, 0), new Vec3d(0, 0, 0));
-        EnderPearlEntity enderPearlEntity = new EnderPearlEntity(new Vec3d(1F/16F, 0.848749995231628396, 0), new Vec3d(0, 0, 0));
+        Explosion explosion = new Explosion(0, 0.061250001192092896, 0, 4.0F);
+        TntEntity tntEntity = new TntEntity(new Vec3d(1F/16F, 0.061250001192093, 0), new Vec3d(0, 0, 0));
+        // EnderPearlEntity enderPearlEntity = new EnderPearlEntity(new Vec3d(1F/16F, 0.848749995231628396, 0), new Vec3d(0, 0, 0));
         System.out.println(explosion.getVelocity(tntEntity, 1.0F));
-        System.out.println(explosion.getVelocity(enderPearlEntity, 1.0F));
-        enderPearlEntity.setVelocity(explosion.getVelocity(enderPearlEntity, 1.0F));
-        enderPearlEntity.tick(10,new Entity.Handler(){
-            public Vec3d[] tickArgument(Vec3d pos, Vec3d vel) {
-                System.out.println(pos + ", " + vel);
-                Vec3d[] result = {pos,vel};
-                return result;
-            }
-        });
-        enderPearlEntity = new EnderPearlEntity(new Vec3d(0D, 10D, 0D), new Vec3d(0D, 0D, 0D));
-        enderPearlEntity.tick(10, new Entity.Handler() {
-            public Vec3d[] tickArgument(Vec3d pos, Vec3d vel) {
-                System.out.println(pos);
-                Vec3d[] result = {pos,vel};
-                return result;
-            }
-        });
-        tntEntity = new TntEntity(new Vec3d(0D, 10D, 0D), new Vec3d(0D, 0D, 0D));
-        tntEntity.tick(10, new Entity.Handler() {
-            public Vec3d[] tickArgument(Vec3d pos, Vec3d vel) {
-                System.out.println(pos);
-                Vec3d[] result = {pos,vel};
-                return result;
-            }
-        });
+        System.out.println(explosion.getVelocityOptimized(tntEntity, 1.0F));
+        // System.out.println(explosion.getVelocity(enderPearlEntity, 1.0F));
+        // enderPearlEntity.setVelocity(explosion.getVelocity(enderPearlEntity, 1.0F));
+        // enderPearlEntity.tick(10,new Entity.Handler(){
+        //     public Vec3d[] tickArgument(Vec3d pos, Vec3d vel) {
+        //         System.out.println(pos + ", " + vel);
+        //         Vec3d[] result = {pos,vel};
+        //         return result;
+        //     }
+        // });
+        // enderPearlEntity = new EnderPearlEntity(new Vec3d(0D, 10D, 0D), new Vec3d(0D, 0D, 0D));
+        // enderPearlEntity.tick(10, new Entity.Handler() {
+        //     public Vec3d[] tickArgument(Vec3d pos, Vec3d vel) {
+        //         System.out.println(pos);
+        //         Vec3d[] result = {pos,vel};
+        //         return result;
+        //     }
+        // });
+        // tntEntity = new TntEntity(new Vec3d(0D, 10D, 0D), new Vec3d(0D, 0D, 0D));
+        // tntEntity.tick(10, new Entity.Handler() {
+        //     public Vec3d[] tickArgument(Vec3d pos, Vec3d vel) {
+        //         System.out.println(pos);
+        //         Vec3d[] result = {pos,vel};
+        //         return result;
+        //     }
+        // });
     }
 }
